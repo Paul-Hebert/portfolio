83687aef5a7221233b5ed73b350a93cad5a611d5
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2019-03-03 14:53:06 -0800

    Use browser-sync to reload page when changes are made.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,12 +2,21 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 const browserSync = require('browser-sync');
 
-gulp.task('build', shell.task('npx eleventy --quiet'));
-
-gulp.task('serve', function() {
-  browserSync.init({
-    server: { baseDir: './_site' }
-  });
+gulp.task('reload', function(done) {
+    browserSync.reload();
+    done();
 });
 
-gulp.task('default', gulp.series('build', 'serve'));
+gulp.task('build', shell.task('npx eleventy --quiet'));
+
+gulp.task('watch', function() {
+    gulp.watch(['src/**'], gulp.series('build', 'reload'));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: { baseDir: './_site' }
+    });
+});
+
+gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
