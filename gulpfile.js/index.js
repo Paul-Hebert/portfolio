5eeabbd261fe05e8c9be43143a7ea41942177fe4
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2019-03-03 18:51:46 -0800

    Configured postcss with gulp

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,13 +1,28 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
+
+const outputDirectory = './_site/'
 
 gulp.task('reload', function(done) {
     browserSync.reload();
     done();
 });
 
-gulp.task('build', shell.task('npx eleventy --quiet'));
+gulp.task('css', () => {
+    return gulp.src('src/**/*.css')
+      .pipe( sourcemaps.init() )
+      .pipe( postcss([ autoprefixer() ]) )
+      .pipe( sourcemaps.write('.') )
+      .pipe( gulp.dest(outputDirectory) )
+});
+
+gulp.task('content', shell.task('npx eleventy --quiet'));
+
+gulp.task('build', gulp.parallel('css', 'content'));
 
 gulp.task('watch', function() {
     gulp.watch(['src/**'], gulp.series('build', 'reload'));
@@ -15,7 +30,7 @@ gulp.task('watch', function() {
 
 gulp.task('serve', function() {
     browserSync.init({
-        server: { baseDir: './_site' }
+        server: { baseDir: outputDirectory }
     });
 });
 
